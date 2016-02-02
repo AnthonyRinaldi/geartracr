94c60b97f906e2df17c23ea7052c2e7445276c48
Anthony Rinaldi <anthonyleerinaldi@gmail.com>
2016-02-01 23:50:13 -0500

    Add form for adding a person record

--- src/AddPersonForm.js
+++ src/AddPersonForm.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
 export default class AddPersonForm extends React.Component {
+  addPerson() {
+    const fname = this.refs.firstname.value
+    const lname = this.refs.lastname.value
+    const color = this.refs.personcolor.value
+
+    // Placeholder
+    console.log(fname, lname, color)
+  }
+
   render() {
     return (
       <div>
         <h2>Add Person Form</h2>
+        <form id='formAddPerson'>
+          <input type="text" ref="firstname" placeholder="First Name" /><br />
+          <input type="text" ref="lastname" placeholder="Last Name" /><br />
+          <input type="color" ref="personcolor" /><br />
+          <button type="button" onClick={this.addPerson.bind(this)}>
+            Add Person
+          </button>
+        </form>
       </div>)
   }
 }
